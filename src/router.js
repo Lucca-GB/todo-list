47da4b2923e0582dbe6880b0e1b0c961c49fec6c
Lucca GB <luccagb@pop-os.localdomain>
2023-05-17 23:55:07 -0300

    desenvolvendo o models de conexao com o db e mexendo no router

--- src/router.js
+++ src/router.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/tasks', (req, res) => {
-    res.status(200).send('router pegando certinho');
-})
+// router.get('/tasks', (req, res) => {
+//     res.status(200).send('router pegando certinho');
+// });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 //importando o arquivo app
 const app = require('./app');
 
+// enjetando variaveis do nosso ambiente env
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3333;
 //rodando o app
-app.listen(3333, () => console.log("rodando"));
+app.listen(3333, () => console.log(`rodando na porta ${PORT}`));
